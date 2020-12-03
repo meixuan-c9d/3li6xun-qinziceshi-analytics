18eabd1f0567839ca91edae8b8020c4fb1b4ac05
meixuan-c9d <mx@c9-d.com>
2020-12-03 08:57:29 +0800

    change token header name

--- middlewares/verification.js
+++ middlewares/verification.js
@@ -1,7 +1,7 @@
 const { response } = require("express");
 
 module.exports = (request, response, next) => {
-  const analyticsToken = request.get('Analytics-Token')
+  const analyticsToken = request.get('X-Analytics-Token')
   if (analyticsToken !== '3/aehCPAp5MZYmwUJx9jGA==') {
     return response.sendStatus(400)
   }
